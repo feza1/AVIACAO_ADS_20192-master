b322af3a76da62393ad72c3b3102e34fcf1d2925
feza1 <felipebassolanger@gmail.com>
2019-08-28 22:06:13 -0300

    Adicionar arquivos de projeto.

---
+++ AviacaoWPF/ViewModel/PessoasViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Aviacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace AviacaoWPF.ViewModel
+{
+   public  class PessoasViewModel
+    {
+        public IList<Pessoa> Pessoas { get; set; }
+
+        public  Boolean PodeRemover  {
+            get {
+                return this.PessoaSelecionada != null;
+            }
+        }
+
+        public ObservableCollection<Pessoa> Pessoa { get; set; }
+
+        public Pessoa PessoaSelecionada { get; set; }
+
+        private ModelAviacao context { get; set; }
+
+        public PessoasViewModel()
+        {
+            context = new ModelAviacao();
+            this.Pessoas =  new ObservableCollection<Pessoa>(context.Pessoas.ToList());
+            this.PessoaSelecionada = context.Pessoas.FirstOrDefault();
+            
+        }
+        public void Salvar()
+        {
+            this.context.SaveChanges();
+        }
+
+        public void Remover()
+        {
+            this.Pessoas.Remove(this.PessoaSelecionada);
+            if(this.PessoaSelecionada.Id != 0)
+            {
+                this.context.Pessoas.Remove(this.PessoaSelecionada);
+            }
+            
+        }
+
+        public void Adicionar()
+        {
+            Pessoa p = new Pessoa();
+            this.Pessoas.Add(p);
+            this.context.Pessoas.Add(p);
+            this.PessoaSelecionada = p;
+
+        }
+    }
+}
